0867f8eb71c8de60cf1f00cdee492b6fc345bc71
SeanNicely <snicely93@gmail.com>
2017-05-13 23:21:05 -0400

    More stability

--- mongoApi.js
+++ mongoApi.js
@@ -1,18 +1,14 @@
 var mongo = require('mongodb').MongoClient;
-var db;
-(function () {
-	mongo.connect('mongodb://127.0.0.1:27017/Hue', (err, database) => {
-		if (err) return console.log(err);
-		db = database;
-	});
-})();
 
 var find = function(collection, query) {
 	var promise = new Promise((resolve,reject) => {
-		db.collection(collection).findOne(query)
-  		.then(data => {
-  			data != null ? resolve(data) : reject(new Error(collection + ' not found'));
-  		});
+		mongo.connect('mongodb://127.0.0.1:27017/Hue', (err, db) => {
+			db.collection(collection).findOne(query)
+  			.then(data => {
+  				console.log(data);
+  				data != null ? resolve(data) : reject(new Error(collection + ' not found'));
+  			});
+		});
 	});
 	return promise;
 }
