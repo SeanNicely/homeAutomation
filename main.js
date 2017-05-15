8c7fd99db5412f9322800c99d876a4e37d9aa6ff
SeanNicely <SeanNicely@users.noreply.github.com>
2017-05-15 12:03:01 -0400

    Update main.js
    Improved REST responses

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ var timers = {
 	"bathroom": null
 };
 
+function respond(response, message, err) {
+	err ? console.log(err) : console.log(message);
+	response.send(message);
+}
+
 function stopClock(room) {
 	if (room === "all") {
 		timers.forEach(rm => {
@@ -22,22 +27,38 @@ app.listen(3000, () => {
 	console.log('Light Automator listening on port 3000!')
 });
 
+// Handles setting Brightness and Saturation attributes
+app.get('/continuous', (req, res) => {
+	stopClock(req.query.room);
+	let lights = hueApi.getLights(req.query.room);
+	hueApi.pluralize(hueApi.setLightState, lights, {req.query.attribute:req.query.percentage})
+	.then(
+		response => respond(res, req.query.room + " lights set to " + req.query.color),
+		err => respond(res, "Problem setting " + req.query.room + " to " + req.query.percentage + " percent " + req.query.attribute, err)
+	);
+});
+
 app.get('/color', (req, res) => {
 	stopClock(req.query.room);
 	let lights = hueApi.getLights(req.query.room);
 	hueApi.getColorXY(req.query.color)
-	.then(color => hueApi.pluralize(hueApi.setLightState, lights, {xy:color}),
-		err => {
-			console.log(err);
-			res.send("Problem setting " + req.query.room + " to " + req.query.color + "\n" + err);
-		})
-	.then(response => res.send(req.query.room + " lights set to " + req.query.color));
+	.then(
+		color => hueApi.pluralize(hueApi.setLightState, lights, {xy:color}),
+		err => respond(res, "Problem setting " + req.query.room + " lights to " + req.query.color", err);
+	)
+	.then(
+		response => respond(res, req.query.room + " lights set to " + req.query.color),
+		err => respond(res, "Problem setting " + req.query.room + " lights to " + req.query.color, err)
+	);
 });
 
 app.get('/off', (req, res) => {
 	stopClock(req.query.room);
 	hueApi.pluralize(hueApi.setLightState, hueApi.getLights(req.query.room), {"on":false})
-	.then(response => res.send(response));
+	.then(
+		response => respond(res, req.query.room + " lights are now off"),
+		err => respond(res, "Problem turning off " + req.query.room + " lights", err)
+	);
 });
 
 app.post('/', (req, res) => {
