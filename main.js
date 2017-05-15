8aa92a456306dd0b114a97e95ee7b0c02af33093
SeanNicely <SeanNicely@users.noreply.github.com>
2017-05-15 11:33:26 -0400

    Update main.js
    Interval clearing for clocks and schedules

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
 var express = require('express');
 var hueApi = require('./HueApi.js');
 var app = express();
-var timers = {};
+var timers = {
+	"livingroom": null,
+	"bedroom": null,
+	"bathroom": null
+};
 
+function stopClock(room) {
+	if (room === "all") {
+		timers.forEach(rm => {
+			if (timers[rm]) clearInterval(timers[rm]);
+		});
+	} else {
+		if (timers[rm]) clearInterval(timers[rm]);
+	}
+	return "Timer cleared for " + room;
+}
 
 app.listen(3000, () => {
-	console.log('Example app listening on port 3000!')
+	console.log('Light Automator listening on port 3000!')
 });
 
-// Temporarily using to test getting colors from Mongo
 app.get('/color', (req, res) => {
-	clearInterval(timers[req.query.room]);
+	stopClock(req.query.room);
 	let lights = hueApi.getLights(req.query.room);
 	hueApi.getColorXY(req.query.color)
 	.then(color => hueApi.pluralize(hueApi.setLightState, lights, {xy:color}),
 		err => {
 			console.log(err);
-			res.send("shit's fucked yo!!!")
+			res.send("Problem setting " + req.query.room + " to " + req.query.color + "\n" + err);
 		})
-	.then(response => res.send(response));
+	.then(response => res.send(req.query.room + " lights set to " + req.query.color));
 });
 
 app.get('/off', (req, res) => {
-	var room;
-	req.query.room ? room = req.query.room : room = "all";
-	hueApi.pluralize(hueApi.setLightState, hueApi.getLights(room), {"on":false})
+	stopClock(req.query.room);
+	hueApi.pluralize(hueApi.setLightState, hueApi.getLights(req.query.room), {"on":false})
 	.then(response => res.send(response));
 });
 
