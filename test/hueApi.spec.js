5762906419ebd58057772e4f04278fdaa8755a70
Sean Nicely <snicely93@gmail.com>
2017-05-23 14:32:26 -0400

    Test cases for getContinuous

--- test/hueApi.spec.js
+++ test/hueApi.spec.js
@@ -2,10 +2,31 @@ var expect = require('chai').expect;
 var hue = require("../lib/hueApi.js");
 
 describe("Hue Lights API", () => {
-	describe("Percentage to Value Conversions", () => {
+	describe("The getContinuous Function", () => {
+		it("should exist", () => {
+			expect(hue.getContinuous).to.exist;
+		})
+
 		it("should convert percentage to brightness value", () => {
-			var brightness = hue.getContinuous({}, "brightness", 100);
-			expect(brightness).to.deep.equal({"bri":255});
+			expect(hue.getContinuous({}, "brightness", 0)).to.deep.equal({"bri":0});
+			expect(hue.getContinuous({}, "brightness", 50)).to.deep.equal({"bri":127});
+			expect(hue.getContinuous({}, "brightness", 100)).to.deep.equal({"bri":255});
+		});
+
+		it("should convert percentage to color temperature value", () => {
+			expect(hue.getContinuous({}, "color temperature", 0)).to.deep.equal({"ct":153});
+			expect(hue.getContinuous({}, "color temperature", 50)).to.deep.equal({"ct":326});
+			expect(hue.getContinuous({}, "color temperature", 100)).to.deep.equal({"ct":500});
+		});
+
+		it("should convert percentage to saturation value", () => {
+			expect(hue.getContinuous({}, "saturation", 0)).to.deep.equal({"sat":0});
+			expect(hue.getContinuous({}, "saturation", 50)).to.deep.equal({"sat":127});
+			expect(hue.getContinuous({}, "saturation", 100)).to.deep.equal({"sat":255});
+		});
+
+		it("should throw an error if attribute is invalid", () => {
+			expect(hue.getContinuous({}, "foo", 0)).to.deep.equal(new Error("foo is not a valid room"))
 		});
 	});
 });
