5cda7a9993cbc6a3f9d312bf6530931fa032622a
SeanNicely <SeanNicely@users.noreply.github.com>
2019-06-19 10:19:39 -0400

    Seed file with examples for home inventory

---
+++ inventory.js
@@ -0,0 +1,96 @@
+{
+  names: [""],
+  location: {
+    room: "",
+    description: ""
+  },
+  attributes: {
+    categories: [""],
+    size: "",
+    brand: "",
+    quantity: ,
+    colors: []
+  },
+  purchaseDetails: {
+    price: ,
+    store: ,
+    date: "YYYY-mm-DD"
+  }
+}
+{
+  names: ["snorkel"],
+  location: {
+    room: "bed",
+    description: "under bed"
+  },
+  attributes: {
+    categories: ["scuba", "recreation", "sporting goods"],
+    brand: "tilos",
+    quantity: 1,
+    colors: ["blue", "clear"]
+  }
+},
+{
+  name: ["fins", "flippers"],
+  location: {
+    room: "bed",
+    description: "under bed"
+  },
+  attributes: {
+    categories: ["scuba", "recreation", "sporting goods"],
+    size: "",
+    brand: "tilos",
+    quantity: 1,
+    colors: ["black","blue"]
+  }
+}
+{
+  name: ["mask", "goggles"],
+  location: {
+    room: "bed",
+    description: "under bed"
+  },
+  attributes: {
+    categories: ["scuba", "recreation", "sporting goods"],
+    size: "",
+    brand: "tilos",
+    quantity: 1,
+    colors: ["clear"]
+  }
+}
+{
+  name: ["wetsuit"],
+  location: {
+    room: "balcony",
+    description: "hanging on heavy bag"
+  },
+  attributes: {
+    categories: ["scuba", "diving", "recreation", "sporting goods"],
+    size: "medium",
+    brand: "henderson",
+    quantity: 1,
+    details: "fire fleece"
+  },
+  purchaseDetails: {
+    price: 300,
+    store: "Columbus Scuba",
+    date: "2019-04-19"
+  }
+}
+{
+  name: ["flashlight", "dive light"],
+  location: {
+    room: "kitchen",
+    description: "counter next to bowl"
+  },
+  attributes: {
+    categories: ["scuba", "diving", "recreation", "sporting goods"],
+    brand: "",
+    quantity: 1
+  },
+  purchaseDetails: {
+    price: 50,
+    store: "Columbus Scuba",
+    date: "2019-04-19"
+  }
+}
