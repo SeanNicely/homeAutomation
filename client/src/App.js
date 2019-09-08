e9dbcd9124983bcc62a2743ffffe2a4cf2b271e9
SeanNicely <snicely93@gmail.com>
2019-09-08 13:55:10 -0400

    Render data to DOM objects

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     state = {
-        data: null
+        ideas: null
     };
 
     componentDidMount() {
         // Call our fetch function below once the component mounts
-        this.callBackendAPI()
-            .then(res => this.setState({ data: res.express }))
+        this.getDateList()
+            .then(res => {
+                console.log("Res: " + JSON.stringify(res));
+                this.setState({ ideas: res });
+            })
             .catch(err => console.log(err));
     }
     // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    callBackendAPI = async () => {
+    getDateList = async () => {
         const response = await fetch('/mary');
         const body = await response.json();
         console.log("Body: ", body);
         if (response.status !== 200) {
             throw Error(body.message)
         }
-        return body;
+        return body.map(idea =>
+            <li key={idea.name}>
+                {JSON.stringify(idea)}
+            </li>
+        );
     };
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                // Render the newly fetched data inside of this.state.data
-                <p className="App-intro">{this.state.data}</p>
+                {/*<header className="App-header">*/}
+                {/*    <img src={logo} className="App-logo" alt="logo" />*/}
+                {/*    <h1 className="App-title">Welcome to React</h1>*/}
+                {/*</header>*/}
+                <ul className="App-intro">{this.state.ideas}</ul>
             </div>
         );
     }
